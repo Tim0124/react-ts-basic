ae8d42a746f8a55a3bdedc87d8307ad0255ad52e
Tim Kao <a0925172969@gmail.com>
2023-11-02 00:07:53 +0800

    feat: add prevnt send for input form feature

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,10 +1,12 @@
-import { FormEvent, useRef } from "react"
+import { FormEvent, useRef, useState } from "react"
 
 type NewGoalProps = {
   onAddGoal: (goal: string, summary: string) => void
 }
 
 export default function NewGoal({onAddGoal}: NewGoalProps) {
+  const [goalError, setGoalError] = useState('')
+  const [summaryError, setSummaryError] = useState('')
   const goal = useRef<HTMLInputElement>(null)
   const summary = useRef<HTMLInputElement>(null)
 
@@ -14,6 +16,17 @@ export default function NewGoal({onAddGoal}: NewGoalProps) {
     const enteredGoal = goal.current!.value
     const enteredSummary = summary.current!.value
 
+    if(enteredGoal.length || enteredSummary.length === 0) {
+      setGoalError('Please enter your goal')
+      setSummaryError('Please enter your summary')
+      setTimeout(()=> {
+        setGoalError('')
+        setSummaryError('')
+      },2000)
+      
+      return
+    }
+
     event.currentTarget.reset()
     onAddGoal(enteredGoal, enteredSummary)
   }
@@ -22,11 +35,11 @@ export default function NewGoal({onAddGoal}: NewGoalProps) {
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input id="goal" type="text" name="goal" ref={goal} />
+        <input id="goal" type="text" name="goal" ref={goal} placeholder={goalError} />
       </p>
       <p>
         <label htmlFor="summary">Short summary</label>
-        <input id="summary" type="text" name="summary" ref={summary} />
+        <input id="summary" type="text" name="summary" ref={summary} placeholder={summaryError} />
       </p>
       <button>Add Goal</button>
     </form>
